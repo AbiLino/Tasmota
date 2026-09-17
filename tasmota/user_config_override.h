25968ede77669ae10ddb6c3b5902f201a873de6a
AbiLino <99500293+AbiLino@users.noreply.github.com>
2026-09-17 22:20:52 +0200

    Add user_config_override.h from sample

---
+++ tasmota/user_config_override.h
@@ -0,0 +1,12 @@
+#ifndef _USER_CONFIG_OVERRIDE_H_
+#define _USER_CONFIG_OVERRIDE_H_
+
+// Sprache auf Deutsch setzen
+#undef MY_LANGUAGE
+#define MY_LANGUAGE DE_DE
+
+// Display und MAX7219 Matrix-Treiber aktivieren
+#define USE_DISPLAY
+#define USE_DISPLAY_MAX7219
+
+#endif // _USER_CONFIG_OVERRIDE_H_
